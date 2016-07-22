88272d3eaa03780ae76cc0c2a81f0c38100c1889
b-goberdhan <brandongob@gmail.com>
2016-07-22 15:29:45 -0600

    smallfixes gem, mysql added

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718185245) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "encrypted_password"
-    t.string   "salt"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
