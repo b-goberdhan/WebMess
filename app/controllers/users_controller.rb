a39182ae2878a929ab999b9c8010feb86ce2f95a
noorh96 <noor.hammad96@hotmail.ca>
2016-07-31 23:05:40 -0600

    Created basic home page for sign up link, basic sign up with no password encryption or validation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+	layout false
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "Sign Up Successful"
+			flash[:color] = "valid"
+			redirect_to(:action => 'home')
+		else
+			flash[:notice] = "Form is Invalid"
+			flash[:color] = "invalid"
+			render ('new')
+		end
+	end
+
+	private 
+	def user_params
+    	params.require(:user).permit(:username, :faculty, :major, :email, :password)
+  	end
+end
