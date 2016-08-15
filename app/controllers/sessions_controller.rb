f73f70cabe603d11df3aaf04e5442e29a60b027f
noorh96 <noor.hammad96@hotmail.ca>
2016-08-15 15:18:02 -0600

    Fixed redirects since bruce is pleb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       session[:user_id]= authorized_user.id
       session[:username]= authorized_user.username
       flash[:notice] = "You are now logged in."
-      redirect_to(:action => 'home')
+      redirect_to(:controller => 'Main',:action => 'home')
     else
       flash[:notice] = "Invalid username/password combination."
       redirect_to(:action => 'login')
