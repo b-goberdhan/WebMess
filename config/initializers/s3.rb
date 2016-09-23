d38e4055bf712caa771d3dd86aeae76914f5b6ad
noorh96 <noor.hammad96@hotmail.ca>
2016-09-23 13:37:44 -0600

    Heroku Fix

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -23,5 +23,5 @@ CarrierWave.configure do |config|
 		:aws_access_key_id 		=> ENV["S3_KEY"],
 		:aws_secret_access_key	=> ENV["S3_SECRET_KEY"]
 	}
-	config.fog_directory => ENV["S3_BUCKET"]
+	config.fog_directory = ENV["S3_BUCKET"]
 end
