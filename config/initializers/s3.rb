7c486b9ca76f033c7bb2cdced5a25fdae096ccf2
noorh96 <noor.hammad96@hotmail.ca>
2016-09-23 13:01:14 -0600

    Heroku Set Up

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -20,8 +20,8 @@ end
 CarrierWave.configure do |config|
 	config.fog_credentials = {
 		:provider				=> 'AWS',
-		:aws_access_key_id 		=> 'AKIAIX2DGP4Z53OOTVNQ',
-		:aws_secret_access_key	=> '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'
+		:aws_access_key_id 		=> ENV["S3_KEY"],
+		:aws_secret_access_key	=> ENV["S3_SECRET_KEY"]
 	}
-	config.fog_directory = 'uvida'
+	config.fog_directory => ENV["S3_BUCKET"]
 end
